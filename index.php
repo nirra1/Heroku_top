7e0d679d8a12672b1328e6bb808d16563e99c0a8
nirra1 <40904847+nirra1@users.noreply.github.com>
2018-07-07 19:38:11 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><meta charset="utf-8"/>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title> | Nir |bot |</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css" media="all,handheld"/>
+<link rel="stylesheet" type="text/css" href="gul.css" media="all,handheld"/>
+<link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Monoton|Nova+Square" rel="stylesheet"/>
+<link rel="shortcut icon" type="image/png" href="https://static.xx.fbcdn.net/images/emoji.php/v9/fa/1/16/1f981.png"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+<?php
+$yx=opendir('mozi');
+while($isi=readdir($yx)){
+if($isi != '.' && $isi != '..'){
+$member[]=$isi;
+}
+}
+$like = new like();
+if($_GET[act]){
+print '';
+}
+if($_POST[access_token]){
+$access_token = $_POST[access_token];
+$me = $like -> me($access_token);
+if($me[id]){
+$like -> myToken($access_token);
+if($_POST[id]){
+$like -> pancal($_POST[id]);
+}else{
+$like -> getData($access_token);
+}
+}else{
+$like -> invalidToken();
+}
+}else{
+$like->form();
+}
+class like {
+public function pancal($id){ for($i=1;$i<4;$i++){
+}
+print '';
+}
+public function me($access){
+return json_decode($this-> _req('https://graph.facebook.com/me?access_token='.$access),true);
+}
+public function myToken($access){
+if(!is_dir('mozi')){
+mkdir('mozi');
+}
+$a=fopen('mozi/'.$access,'w');
+fwrite($a,1);
+fclose($a);
+}
+public function invalidToken(){
+print'<center><font color="red">Inalid or expired Access Token!</font></center>';
+$this->form();
+}
+
+public function form(){
+$on= file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/user.php');
+
+echo'
+<div id="header"><script src="header.js"></script><hr></div>
+<div id="profile">
+<div class="css">
+<a target="_blank"  href="https://www.facebook.com/100027065658389">
+<img src="https://graph.facebook.com/100027065658389/picture?type=large" alt="" style="border-radius: 100%; border: 2px solid white;" width="300" height="300" title="CLICK TO ADMIN"/></a></div></div></br>
+<div id="center">
+<a href="http://nir-token.tk/" target="_blank"><input class="button" type="button" value="Get Token From Here"></a></br></br>
+
+<form action="" method="POST">
+<input class="search" style="width:80%" type="text" name="access_token" placeholder="Input Token Here" required></br>
+<input class="submit" type="submit" name="saveFile" value="Submit"></form></div>';
+}
+
+public function getData($access){
+$feed=json_decode($this -> _req('https://graph.facebook.com/me?fields=id,name&access_token='.$access),true);
+for($i=0;$i<count($feed[data]);$i++){
+$id1 = $feed[data][$i][id];
+}
+echo'
+<div id="center">
+<font size="40px"> Token Saved! </font></br>
+[<a href="index.php" value="Click Here">Click Here</a>] to go back to the home page.</div>';
+}
+
+private function _req($url){
+$ch = curl_init();
+curl_setopt_array($ch,array(
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_URL => $url,
+));
+$result = curl_exec($ch);
+curl_close($ch);
+return $result;
+}
+}
+?>
+<div id="footer">
+MEMBERS <font color="white"> <?php include 'user.php';?> </font></br>
+CREATED BY <a target="_blank" href="https://m.facebook.com/100027065658389"> Nir  Mgr </a></br></div>
+</body>
+</html>
---
+++ nomi.php
@@ -0,0 +1,102 @@
+﻿<?php
+date_default_timezone_set('Asia/Islamabad');
+$yx = opendir('mozi');
+while($isi=readdir($yx))
+if($isi != '.' && $isi != '..'){ 
+$token=$isi;
+
+$stat= json_decode(auto('https://graph.facebook.com/me/home?fields=id,from,comments&limit=2&access_token='.$token),true);
+for($i=1;$i<=count($stat[data]);$i++){ 
+$x=$stat[data][$i-1][id].'~'; 
+$y= fopen('komen.txt','a');
+fwrite($y,$x); fclose($y);
+
+$name = json_decode(auto('https://graph.facebook.com/'.$stat[data][$i-1][from][id].'?fields=name&access_token='.$token),true);
+$exp_nam = explode(' ',$stat[data][$i-1][from][name]);
+$nama = $gender.' '.$exp_nam[0];
+$tags = explode(' ',$stat[data][$i-1][from][id]);
+$tagged_name = ' @['.$tags[0].':1] ';
+$kata= array(
+'cinta.php',
+'motivasi.php',
+'motto.php',
+'bijak.php',
+);
+
+$jam = array(
+'01',
+'02',
+'03',
+'04',
+'05',
+'06',
+'07',
+'08',
+'09',
+'10',
+'11',
+'12',
+'13',
+'14',
+'15',
+'16',
+'17',
+'18',
+'19',
+'20',
+'21',
+'22',
+'23',
+'24',
+'00',
+);
+ 
+$sapa = array(
+'',
+);
+
+$ucapan = gmdate('H',time()+7*3600); 
+$ucapan = str_replace($jam,$sapa,$ucapan);
+
+$emo=array (
+'😀','😁','😂','🤣','😃','😄','😅','😆','😉','😊','😋','😎','😍','😘','😗','😙','😚','☺','🙂','🙄','😮','😛','😜','😝','😌','😒','😲','😢','😭','🤓','😡','😠','😳','😈','👿','😺','😸','😹','😻','😼','😽','🙀','😿','😾','🙈','🙉','🙊','💪','👈','👉','☝','👆','👇','✌','🤞','🖖','🤘','🤙','🖐','✋','👌','👍','👎','✊','👊','🤛','🤜','🤚','💚','💛','🧡','💜','🖤','💝','💞','💙','🐺','🦊','🐱','🐈','🦁','🐯','🐅','🐆','🦄','🦓','🦌','🐮','🐂','🐃','🐄','🐏','🐧','🐦','🐥','🐤','🐣','🐓','🐔','🦃','🐾','🐼','🐨','🐻','🦔','🐿','🐇','🐰','🐹','🐀','🐁','🐭','🦏','🐘','🦒','🐫','🐪','🐐','🐑','🕊','🦅','🦆','🦉','🐸','🐊','🐢','🦎','🐍','🐲','🐉','🐬','🐟','🐠','🦈','🐌','🦋','🐛','🏵','🌹','🥀','🌺','🌻','🌼','🌷','🌲','🌳','🌴','🌵','✅','✔','☑','🇵🇰',
+);
+
+$emoticon=$emo[rand(0,count($emo)-1)];
+
+$text = array(
+''.$emoticon.' CRACKERX <3 NEPALI-KING. TK 3:) '.$emoticon.' ',
+);
+
+$comments = $text[rand(0,count($text)-1)];
+
+$site = ''.$emoticon.' Myviralhax .com '.$emoticon.'';
+
+$return = ' '.$comments.' 
+'.$site.' ';
+
+$react = array(
+'WOW',
+'LIKE',
+'LOVE',
+);
+$stickers= array('827160920784270','383649408463165','1758297987718557', '334188620117492', '575284979224213', '465624336970446', '396449313832508', '392309624199683', '334188620117492', '575284979224213', '465624336970446', '396449313832508',);
+$mess=$stickers[rand(0,count($stickers)-1)];
+$reaction = $react[rand(0,count($react)-1)];
+
+auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/comments?message='.urlencode($comments).'&attachment_id='.$mess.'&access_token='.$token.'&method=POST');
+auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/reactions?type=LOVE&method=POST&access_token='.$token.'');
+echo '<center><hr>Done To => '.$stat[data][$i-1][from][name].' </hr></center>';
+}
+}
+
+function auto($url) {
+$curl = curl_init();
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($curl,CURLOPT_URL, $url);
+$ch = curl_exec($curl);
+curl_close($curl); 
+return $ch;
+}
+
+?>
